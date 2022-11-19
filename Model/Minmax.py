7fa0b2fe96aea4bf6cecf9668cf0c5d2043c2042
Mahmoud ElShenawy <Mahmoudelshenawy009@gmail.com>
2022-11-19 20:49:31 +0200

    -Added AI agent to play and made the implementation of minmax multiprocess

--- Model/Minmax.py
+++ Model/Minmax.py
@@ -1,38 +1,60 @@
+import concurrent.futures
 import math
-import Board
+from Model.Board import Board
+import time
 
-
-class controller:
+class Minmax:
     def __init__(self):
-        self.b = Board.Board()
+        self.b = Board()
 
     def eval_heuristic(self, current_board):
         x= self.b.get_4_score(current_board)
         return x[0]-x[1]
 
-    def minimax(self, current_board : tuple , depth, maximizingPlayer):  # current_board fe elawl tb3tlaha tuple (el board,-1)
+    def minmax_mutli(self, current_board, depth):
+        neighbours=self.b.get_neighbours(current_board[0],True)
+        t = []
+        for n in neighbours:
+            t.append((n, depth - 1,False))
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            results = executor.map(self.minimax,t)
+        max=-math.inf
+        j=0
+        for r,i in zip(results,range(len(t))):
+            if r[1] > max:
+                max=r[1]
+                j=r
+
+        print(j)
+        return j
+
+    def minimax(self,param ):
+        current_board , depth, maximizingPlayer=param # current_board fe elawl tb3tlaha tuple (el board,-1)
         if depth == 0:  # or game over in position
             return None, self.eval_heuristic(current_board[0])
 
         if maximizingPlayer:  # 0
             chosen_child, maxEval = None, -math.inf
             neighbours = self.b.get_neighbours(current_board[0], 0)
-            print(neighbours[0])
+            # print(neighbours[0])
             for child in neighbours:  #######de lazm tb2a el neighbours bto3 current_board fa lazm nst5dm get_neighbours 34an t4t8l
-                print(bin(current_board[0]))
-                print(type(child[0]),type(child[1]))
-                returned_child, eval = self.minimax(child, depth - 1, False)
+                # print(bin(current_board[0]))
+                # print(type(child[0]),type(child[1]))
+                returned_child, eval = self.minimax((child, depth - 1, False))
                 if eval > maxEval:
                     chosen_child, maxEval = child, eval
+                    # print(chosen_child)
+
             return chosen_child, maxEval
 
         else:  # minimizer 1
             chosen_child, minEval = None, math.inf
             neighbours=self.b.get_neighbours(current_board[0], 1)
             for child in neighbours:  #######de lazm tb2a el neighbours bto3 current_board fa lazm nst5dm get_neighbours 34an t4t8l
-                returned_child, eval, = self.minimax(child, depth - 1, True)
+                returned_child, eval, = self.minimax((child, depth - 1, True))
                 if eval < minEval:
                     chosen_child, minEval = child, eval
+                    # print(chosen_child)
             return chosen_child, minEval
 
     def minimax_alpha_beta(self, current_board :tuple, depth, alpha, beta, maximizingPlayer):  # current_board fe elawl tb3tlaha tuple (el board,-1)
@@ -61,13 +83,17 @@ class controller:
                     break
             return chosen_child, minEval
 if __name__ == '__main__':
-    c= controller()
+    c= Minmax()
    # b = Board()
     i=0
     y:tuple =(c.b.board,-1)
-    print(type(y[0]))
-    x=c.minimax(y,4,False)
-    print(x[0][1])
-    print(bin(x[0][0]))
+    # print(type(y[0]))
+    x1=time.time()
+    c.minmax_mutli(y, 6)
+    # x=c.minimax((y,7,False))
+    x2=time.time()
+    print(x2-x1)
+    # print(x[0][1])
+    # print(bin(x[0][0]))
    # print(c.b.get_neighbours(int("000000000000000000000000000000000000000000000000000000010000001",base=2),1))
    # print(c.b.get_4_score(int("110011111110110101110010010110010101110001000110100000110011000", base=2)))
